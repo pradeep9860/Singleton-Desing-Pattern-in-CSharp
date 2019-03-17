bb34b2eb62e6897168fba2418a63d1e1df6698d1
Pradeep Raj Thapaliya <thapaliyapradeep@gmail.com>
2019-03-17 19:08:01 +0545

    adding multiple thread to run at once

--- SingletoneDesignPatternDemo/Program.cs
+++ SingletoneDesignPatternDemo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using SingletonDesignPatternDemo.SingletonClasses;
 using System;
+using System.Threading.Tasks;
 
 namespace SingletonDesignPatternDemo
 {
@@ -7,19 +8,35 @@ namespace SingletonDesignPatternDemo
     {
         static void Main(string[] args)
         {
+
+            //Parallel invocation test for thread safety
+
+
+            Parallel.Invoke(
+                () => FromConfig1(),
+                () => FromConfig2()
+            );
+
+            //FromConfig1();  
+            //FromConfig2();
+
+            Console.ReadKey();
+        }
+
+        private static void FromConfig2()
+        {
+            //now we are going to get the instance to new varialble and see whats happens
+            ApplicationConfig config2 = ApplicationConfig.Instance;
+            config2.PrintConfigValue("config2"); // this will print the config value that is set by the config1 instance
+        }
+
+        private static void FromConfig1()
+        {
             //get the instance of an singletone class and set the config value
 
             ApplicationConfig config1 = ApplicationConfig.Instance;
             config1.SetConfigValue(8);
             config1.PrintConfigValue("config1");
-
-
-            //now we are going to get the instance to new varialble and see whats happens
-            ApplicationConfig config2 = ApplicationConfig.Instance;
-            config2.PrintConfigValue("config2"); // this will print the config value that is set by the config1 instance
-
-
-            Console.ReadKey();
         }
     }
 }
