1ec1d0724011ee3171ac50f5c7b97dd4424e4e57
Pradeep Raj Thapaliya <thapaliyapradeep@gmail.com>
2019-03-17 18:29:41 +0545

    single implementation

---
+++ SingletoneDesignPatternDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using SingletoneDesignPatternDemo.SingletoneClasses;
+using System;
+
+namespace SingletoneDesignPatternDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //get the instance of an singletone class and set the config value
+
+            ApplicationConfig config1 = ApplicationConfig.Instance;
+            config1.SetConfigValue(8);
+            config1.PrintConfigValue("config1");
+
+
+            //now we are going to get the instance to new varialble and see whats happens
+            ApplicationConfig config2 = ApplicationConfig.Instance;
+            config2.PrintConfigValue("config2"); // this will print the config value that is set by the config1 instance
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SingletoneDesignPatternDemo/SingletoneClasses/ApplicationConfig.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletoneDesignPatternDemo.SingletoneClasses
+{
+    public sealed class ApplicationConfig
+    { 
+        /*
+        * Private property initilized with null
+        * ensures that only one instance of the object is created
+        * based on the null condition
+        */
+        private static ApplicationConfig instance = null;
+
+        /*
+       * public property is used to return only one instance of the class
+       * leveraging on the private property
+       */
+        public static ApplicationConfig Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new ApplicationConfig();
+                return instance;
+            }
+        }
+
+        /*
+        * Private constructor ensures that object is not
+        * instantiated other than with in the class itself
+        */
+        private ApplicationConfig()
+        {
+            ConfigValue = 1; 
+        }
+         
+        private int ConfigValue { get; set; }
+
+        /*
+       * Public method which can be invoked through the singleton instance to print current config value
+       */
+        public void PrintConfigValue(string key)
+        {
+            Console.WriteLine($"Config Value from {key}: {ConfigValue}");
+        }
+
+        /*
+      * Public method which can be invoked through the singleton instance to set new value to config property
+      */
+        public void SetConfigValue(int newValue)
+        {
+            this.ConfigValue = newValue;
+        }
+    }
+}
